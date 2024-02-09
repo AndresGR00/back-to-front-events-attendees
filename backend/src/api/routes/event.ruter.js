039423bfa6d5b83840f50e2ffe4b26fbbbc8f578
AndresGR00 <angare97@gmail.com>
2024-02-09 18:45:48 +0100

    Project Finished

--- backend/src/api/routes/event.ruter.js
+++ backend/src/api/routes/event.ruter.js
@@ -8,7 +8,7 @@ eventRouter.get('/events/:id', eventController.getEventById);
 eventRouter.post('/user/events', [isAuth], uploadPosters.fields([{name: "poster"}]), eventController.createEvent);
 eventRouter.patch('/update-poster/:id', uploadPosters.fields([{name: "poster"}]), eventController.updatePoster);
 eventRouter.delete('/delete-event/:id', [isAdmin], eventController.removeEvent);
-eventRouter.post('/user/attendees/:eventId/:userId', [isAuth], eventController.userConfirmation);
+eventRouter.post('/user/attendees/:eventId/:userId', /* [isAuth], */ eventController.userConfirmation);
 eventRouter.post('/attendant/attendees/:eventId/:attendantId', eventController.attendantConfirmation);
 
 module.exports = eventRouter;
